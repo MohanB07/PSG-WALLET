0e410d84a6a19566dfdd983a66a0218133ff860d
Aparna-xxx <aparnaramanathan14@gmail.com>
2024-03-05 14:33:04 +0530

    Mohan msg

--- PSG-WALLET/screens/RegisterScreen.js
+++ PSG-WALLET/screens/RegisterScreen.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native'
 export default function RegisterScreen() {
     return (
     <View style={styles.container}>
-    <Text style={styles.text}>RegisterScreen</Text>
+    <Text style={styles.text}>Mohan is great thalaiva engala aasirvadham panunngaaaaa</Text>
     </View>
     )
 }
